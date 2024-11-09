7cba72e8afda89bf7cb2d226701695adcba035cc
unknown <akashkumar200124@gmail.com>
2024-11-09 22:45:42 +0530

    comments route and controller completed

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,5 +1,143 @@
-
+import asynHandler from "../utils/asynHandler.js"
+import { Video } from "../models/video.modal.js"
+import { apiError } from "../utils/apiError.js"
+import { Comment } from "../models/comment.modal.js"
+import { ApiResponse } from "../utils/apiResponse.js"
 //1. get video comments 
 // 2. add comment 
 // 3. update comment 
 // 4. delete comment 
+
+const addComments = asynHandler(async (req, res) => {
+
+    const { videoId, content } = req.body;
+
+    if (!videoId || !content) {
+        throw new apiError(400, "video id and content is required")
+    }
+
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+        throw new apiError(400, "video not found")
+    }
+
+    const comment = await Comment.create({
+        video: videoId,
+        content: content,
+        owner: req.user._id
+    });
+
+    res.status(200)
+        .json(
+            new ApiResponse(
+                200,
+                comment,
+                "comment added successfully"
+            )
+        )
+
+}
+)
+
+const updateComment = asynHandler(async (req, res) => {
+
+    const { _id, content } = req.body;
+
+    if (!_id || !content) {
+        throw new apiError(400, "comment id and content is required")
+    }
+    
+    const comment = await Comment.findById(_id);
+
+    if (!comment) {
+        throw new apiError(400, "comment not found")
+    }
+
+    if(comment.owner.toString() !== req.user._id.toString()) {
+        throw new apiError(401, "Unauthorized request")
+    }
+
+    const newcomment = await Comment.findByIdAndUpdate(_id,
+        {
+            content
+        },
+        {
+            new: true
+        }
+    )
+
+    res.status(200)
+     .json(
+        new ApiResponse(
+            200,
+            newcomment,
+            "comment updated successfully"
+        )
+     )
+})
+
+const deleteComment = asynHandler(async (req, res) => {
+
+    const { _id } = req.body; 
+
+    if (!_id) {
+        throw new apiError(400, "comment id is required")
+    }
+
+    const comment = await Comment.findById(_id);
+
+    if (!comment) {
+        throw new apiError(400, "comment not found")
+    }
+
+    if(comment.owner.toString() !== req.user._id.toString()) {
+        throw new apiError(401, "Unauthorized request")
+    }
+
+    const newcomment = await Comment.findByIdAndDelete(_id);
+
+    res.status(200)
+     .json(
+        new ApiResponse(
+         200,
+         "comment deleted successfully"
+        )
+     )
+
+})
+
+const getAllVideoComments = asynHandler(async (req, res) => {
+
+    const { videoId } = req.body;
+
+    if (!videoId) {
+        throw new apiError(400, "video id is required") 
+    }
+    
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+
+    // Calculate the starting point for fetching data
+    const offset = (page - 1) * limit;
+
+    const comments = await Comment.find({video : videoId})
+        .skip(offset)
+        .limit(limit)
+        .sort({createdAt: -1});
+    
+    res.status(200)
+     .json(
+        new ApiResponse(
+         200,
+         comments,
+         "comments fetched successfully"
+        )
+     )
+})
+
+export { addComments,
+         updateComment,
+         deleteComment,
+         getAllVideoComments
+ }
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,12 +1,23 @@
 import { Router } from "express";
-// import { verifyjwt } from "../middlewares/auth.middleware.js";
+import { verifyjwt } from "../middlewares/auth.middleware.js";
+
+import {addComments,
+        updateComment,
+        getAllVideoComments,
+        deleteComment
+} from "../controllers/comment.controller.js"
 
 
 const router = Router();
 
-// router.use(verifyjwt) ; // apply verifyjwt middleware to all routes in this field 
+router.use(verifyjwt) ; // apply verifyjwt middleware to all routes in this field 
 
-// router.route("/:videoId").get(getVideoComments).post(addComments);
+// all routes are secure here
+router.route("/add-comment").post(addComments);
+router.route("/update-comment").put(updateComment);
+router.route("/get-video-comments").get(getAllVideoComments);
+router.route("/delete-comment").delete(deleteComment);
+
 
 
 
