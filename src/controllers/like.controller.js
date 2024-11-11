d17f88d53b28b03ef26e321b78e91cf26d81034a
unknown <akashkumar200124@gmail.com>
2024-11-11 22:50:34 +0530

    like all feature with testing completed here

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -135,38 +135,38 @@ const getAllLikedVideo = asynHandler(async (req, res) => {
 
     const videoIds = await Like.aggregate([
         {
-          $match: {
-            likedBy: userId // Filter by user ID
-          }
+            $match: {
+                likedBy: userId // Filter by user ID
+            }
         },
         {
-          $project: {
-            video: 1 // Only project the "video" field
-          }
+            $project: {
+                video: 1 // Only project the "video" field
+            }
         },
         {
-          $unwind: "$video" // Unwind to get each video ID as a separate document entry
+            $unwind: "$video" // Unwind to get each video ID as a separate document entry
         },
         {
-          $group: {
-            _id: null, // Group all video IDs together
-            allVideoIds: { $addToSet: "$video" } // Collect all unique video IDs
-          }
+            $group: {
+                _id: null, // Group all video IDs together
+                allVideoIds: { $addToSet: "$video" } // Collect all unique video IDs
+            }
         }
-      ]);
-  
-      // If no video IDs are found, send an empty array
-      const result = videoIds.length > 0 ? videoIds[0].allVideoIds : [];
+    ]);
 
-      const data = await Promise.all(
+    // If no video IDs are found, send an empty array
+    const result = videoIds.length > 0 ? videoIds[0].allVideoIds : [];
+
+    const data = await Promise.all(
         result.map(async (videoId) => {
-          const video = await Video.findById(videoId);
-          return video || null; // Ensure each call returns either a video or null
+            const video = await Video.findById(videoId);
+            return video || null; // Ensure each call returns either a video or null
         })
-      );
-      
-      // Filter out any null values (in case some video IDs didn't match a document)
-      const result1 = data.filter((video) => video !== null);
+    );
+
+    // Filter out any null values (in case some video IDs didn't match a document)
+    const result1 = data.filter((video) => video !== null);
 
     res.status(200)
         .json(
@@ -178,6 +178,212 @@ const getAllLikedVideo = asynHandler(async (req, res) => {
         )
 
 })
+
+const likeComment = asynHandler(async (req, res) => {
+
+    const { commentId } = req.body;
+    const userId = req.user._id;
+
+    if (!commentId) {
+        throw new apiError(400, "comment id is required")
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(commentId)) {
+        throw new apiError(400, "comment not found")
+    }
+
+    try {
+    let like = await Like.findOne({ likedBy: userId });
+
+    if (like) {
+        // Check if the videoId is already liked
+        if (!like.comment.includes(commentId)) {
+            like.comment.push(commentId); // Add video ID to the video array
+            await like.save(); // Save changes to the document
+            return res.status(200).json(
+                new ApiResponse(
+                    200,
+                    "comment liked successfully"
+                )
+            );
+        } else {
+            return res.status(400).json(
+                new ApiResponse(
+                    400,
+                    "comment already liked"
+                ));
+        }
+    } else {
+        // Create a new Like document if one doesn't exist for this user
+        like = new Like({
+            likedBy: userId,
+            comment: [commentId]
+        });
+        await like.save();
+
+        res.status(201)
+            .json(
+                new ApiResponse(
+                    201,
+                    "comment liked successfully"
+                )
+            )
+    }
+} catch (error) {
+    res.status(500).json({ message: "Failed to like comment", error });
+}
+})
+
+const likeTweet = asynHandler(async (req, res) => {
+
+    const { tweetId } = req.body;
+    const userId = req.user._id;
+
+    if (!tweetId) {
+        throw new apiError(400, "Tweet id is required")
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(tweetId)) {
+        throw new apiError(400, "Tweet not found")
+    }
+
+    try {
+    let like = await Like.findOne({ likedBy: userId });
+
+    if (like) {
+        // Check if the videoId is already liked
+        if (!like.tweet.includes(tweetId)) {
+            like.tweet.push(tweetId); // Add video ID to the video array
+            await like.save(); // Save changes to the document
+            return res.status(200).json(
+                new ApiResponse(
+                    200,
+                    "Tweet liked successfully"
+                )
+            );
+        } else {
+            return res.status(400).json(
+                new ApiResponse(
+                    400,
+                    "Tweet already liked"
+                ));
+        }
+    } else {
+        // Create a new Like document if one doesn't exist for this user
+        like = new Like({
+            likedBy: userId,
+            tweet: [tweetId]
+        });
+        await like.save();
+
+        res.status(201)
+            .json(
+                new ApiResponse(
+                    201,
+                    "Tweet liked successfully"
+                )
+            )
+    }
+} catch (error) {
+    res.status(500).json({ message: "Failed to like Tweet", error });
+}
+})
+
+const unlikeComment = asynHandler(async (req, res) => {
+    const { commentId } = req.body;
+
+    if (!commentId) {
+        throw new apiError(400, "Comment id is required")
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(commentId)) {
+        throw new apiError(400, "comment not found")
+    }
+
+    const like = await Like.findOne({ likedBy: req.user._id });
+
+    if (like) {
+
+        if (like.comment.includes(commentId)) {
+
+            const index = like.comment.indexOf(commentId);
+            like.comment.splice(index, 1);
+            await like.save();
+
+            res.status(200).json(
+                new ApiResponse(
+                    200,
+                    "comment unliked successfully"
+                )
+            )
+        } else {
+            res.status(400).json(
+                new ApiResponse(
+                    400,
+                    "comment not unliked"
+                )
+            )
+        }
+    } else {
+        res.status(400).json(
+            new ApiResponse(
+                400,
+                "comment not liked yet by user"
+            )
+        )
+    }
+
+
+})
+
+const unlikeTweet = asynHandler(async (req, res) => {
+    const { tweetId } = req.body;
+
+    if (!tweetId) {
+        throw new apiError(400, "Comment id is required")
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(tweetId)) {
+        throw new apiError(400, "Tweet not found")
+    }
+
+    const like = await Like.findOne({ likedBy: req.user._id });
+
+    if (like) {
+
+        if (like.tweet.includes(tweetId)) {
+
+            const index = like.tweet.indexOf(tweetId);
+            like.tweet.splice(index, 1);
+            await like.save();
+
+            res.status(200).json(
+                new ApiResponse(
+                    200,
+                    "Tweet unliked successfully"
+                )
+            )
+        } else {
+            res.status(400).json(
+                new ApiResponse(
+                    400,
+                    "Tweet not unliked"
+                )
+            )
+        }
+    } else {
+        res.status(400).json(
+            new ApiResponse(
+                400,
+                "Tweet not liked yet by user"
+            )
+        )
+    }
+
+
+})
+
+
 
 // const likeComment = asynHandler(async (req, res) => {
 
@@ -285,8 +491,10 @@ const getAllLikedVideo = asynHandler(async (req, res) => {
 export {
     likeVideo,
     unlikeVideo,
-    getAllLikedVideo
-    // likeComment,
-    // likeTweet,
-    // getUserLikedVideos
+    getAllLikedVideo,
+    likeComment,
+    unlikeComment,
+    likeTweet,
+    unlikeTweet
+    
 }
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,7 +1,13 @@
 
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import {likeVideo ,unlikeVideo,getAllLikedVideo} from "../controllers/like.controller.js";
+import {likeVideo,
+        unlikeVideo,
+        getAllLikedVideo,
+        likeComment,
+        unlikeComment,
+        likeTweet,
+        unlikeTweet} from "../controllers/like.controller.js";
 
 const router = Router();
 
@@ -9,8 +15,10 @@ router.use(verifyJwt); // add middleware to secure all the routes below
 // all routes are secure here 
 router.route("/likeVideo").post(likeVideo);
 router.route("/unlikeVideo").post(unlikeVideo);
-// router.route("/likeComment").post(likeComment);
-// router.route("/likeTweet").post(likeTweet);
+router.route("/likeComment").post(likeComment);
+router.route("/unlikeComment").post(unlikeComment);
+router.route("/likeTweet").post(likeTweet);
+router.route("/unlikeTweet").post(unlikeTweet);
 router.route("/getUserLikedVideos").get(getAllLikedVideo);
 
 export default router;
