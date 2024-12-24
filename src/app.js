dc08aa15239ec26c73ef03ee8ba702faabd345d6
unknown <akashkumar200124@gmail.com>
2024-12-24 21:22:26 +0530

    socket

--- src/app.js
+++ src/app.js
@@ -1,23 +1,53 @@
-import express from "express";
+// import express from "express";
 import cookieParser from "cookie-parser";
 import cors from 'cors';
+// import { Server } from "socket.io";
+// import http from "http";
+
+import express from "express";
+import { Server } from "socket.io";
+import http from "http";
+
+
 
 
 const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("send_message", (data) => {
+    console.log("Message received:", data);
+    io.emit("receive_message", data); // Broadcast message
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
 
 app.use(cors({
-  
-    origin: true,
-    credentials: true,
+  origin: "http://localhost:5173",
+  credentials: true,
 
 }))
 
-console.log("process.env.CORS_ORIGIN",process.env.CORS_ORIGIN)
+
+console.log("process.env.CORS_ORIGIN", process.env.CORS_ORIGIN)
 
 // to accept json in body 
 app.use(express.json({ "limit": "16kb" }))
 // to get the data from the url 
-app.use(express.urlencoded({extended : true , limit : "16kb"}))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 //public assets 
 app.use(express.static("public"))
 //to accept cookie
@@ -28,19 +58,19 @@ app.use(cookieParser());
 import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import commentRouter from "./routes/comment.routes.js";
-import  likeRouter from "./routes/like.routes.js";
+import likeRouter from "./routes/like.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
-import   subscriptionRouter  from "./routes/sunscription.routes.js";
+import subscriptionRouter from "./routes/sunscription.routes.js";
 
 //routes decleartion 
-app.use("/api/v1/users",userRouter);
-app.use("/api/v1/videos",videoRouter);
-app.use("/api/v1/comment",commentRouter);
-app.use("/api/v1/like",likeRouter);
-app.use("/api/v1/tweet",tweetRouter);
-app.use("/api/v1/playlist",playlistRouter);
-app.use("/api/v1/subscription",subscriptionRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/like", likeRouter);
+app.use("/api/v1/tweet", tweetRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/subscription", subscriptionRouter);
 
 
 
@@ -52,14 +82,14 @@ app.use("/api/v1/subscription",subscriptionRouter);
 
 
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    
-    res.status(err.statusCode || 500).json({
-      success: err.success,
-      message: err.message,
-      errors: err.errors,
-      data: err.data
-    });
+  console.error(err.stack);
+
+  res.status(err.statusCode || 500).json({
+    success: err.success,
+    message: err.message,
+    errors: err.errors,
+    data: err.data
   });
+});
 
 export { app }
